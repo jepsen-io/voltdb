310868cb809e118573e85b7f7c35163118e5bd92
Aphyr <aphyr@aphyr.com>
2016-06-27 12:24:36 -0500

    Jepsen 0.1.2

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.3"]
-                 [jepsen "0.1.1-SNAPSHOT"]
-                 [knossos "0.2.8-SNAPSHOT"]
+                 [jepsen "0.1.2"]
                  [org.clojure/data.xml "0.0.8"]
                  [org.voltdb/voltdbclient "6.2"]]
   :jvm-opts ["-Xmx8g"
