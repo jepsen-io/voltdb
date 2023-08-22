1afe36d34b2e56453767aa63b2b1f4c12dc3a7b8
Aphyr <aphyr@aphyr.com>
2023-08-22 15:44:44 -0500

    project.clj: even more JDK add-opens...

--- project.clj
+++ project.clj
@@ -6,11 +6,19 @@
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [jepsen "0.3.3"]
                  [org.clojure/data.xml "0.0.8"]
-                 [org.voltdb/voltdbclient "6.2"]]
+                 [org.voltdb/voltdbclient "12.1.0"]]
   :jvm-opts ["-Xmx8g"
              "-XX:MaxInlineLevel=32"
              "-server"
-             "--add-opens" "java.base/sun.nio.ch=ALL-UNNAMED"]
+             "--add-opens" "java.base/java.lang=ALL-UNNAMED"
+             "--add-opens" "java.base/sun.nio.ch=ALL-UNNAMED"
+             "--add-opens" "java.base/java.net=ALL-UNNAMED"
+             "--add-opens" "java.base/java.nio=ALL-UNNAMED"
+             "--add-opens" "java.base/sun.net.www.protocol.http=ALL-UNNAMED"
+             "--add-opens" "java.base/sun.net.www.protocol.https=ALL-UNNAMED"
+             "--add-opens" "java.base/sun.net.www.protocol.file=ALL-UNNAMED"
+             "--add-opens" "java.base/sun.net.www.protocol.ftp=ALL-UNNAMED"
+             "--add-opens" "java.base/sun.net.www.protocol.jar=ALL-UNNAMED"]
   :main jepsen.voltdb.runner
   :aot  [jepsen.voltdb.runner
          clojure.tools.logging.impl])
